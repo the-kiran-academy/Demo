84776645b3fada7e05a92eae5d91b7b9fc72a0b4
ram-chadar <javaprojectwork2715@gmail.com>
2024-06-05 10:32:38 +0530

    intial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		LOGGER.error("Message logged at ERROR level");
+		LOGGER.warn("Message logged at WARN level");
+		LOGGER.info("Message logged at INFO level");
+		LOGGER.debug("Message logged at DEBUG level");
+	}
+
+	@RequestMapping("/")
+	public String welcome() {
+		return "Hello World!!";
+	}
+
+}
