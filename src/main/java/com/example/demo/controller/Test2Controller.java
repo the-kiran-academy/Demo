55ea80b0cce4ad3ee764f7a95d69991c5abd0004
rushikeshb30 <rushikesh.bhadage30@gmail.com>
2025-04-10 17:11:14 +0530

    Rushikesh

---
+++ src/main/java/com/example/demo/controller/Test2Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test2Controller {
+
+	@GetMapping("/rushikesh")
+	public String demoApi() {
+		return "rushikesh";
+	}
+}
